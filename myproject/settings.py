2e7cee580e2a41918dc49c95262fcc60111dbb25
palakgoyal95 <palakgoyal29062002@gmail.com>
2025-10-26 03:38:41 +0530

    changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,4 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / "staticfiles" # Directory where static files will be collected
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ # Directory where static files will be collected
